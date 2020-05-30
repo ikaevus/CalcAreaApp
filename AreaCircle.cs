0e84751e3d69b6c85ce30807a80f9b6fc58a8c3e
ikaevus <62504580+ikaevus@users.noreply.github.com>
2020-05-30 18:10:49 +0300

    Update AreaCircle.cs

--- AreaCircle.cs
+++ AreaCircle.cs
@@ -5,7 +5,8 @@ namespace CalcAreaApp
     public class AreaCircle
     {
         public double R;
-       public static double Square(double R)
+        
+        public static double Square(double R)
         {
             return Math.PI * Math.Pow(R, 2);
         }    
